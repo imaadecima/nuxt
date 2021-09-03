f2d0b7e7f6fa32c79526933963fcff90b88f5bf7
unknown <imaadecima@gmail.com>
2021-09-03 16:07:17 -0300

    Add nuxt vercel config 14

--- api/index.js
+++ api/index.js
@@ -3,13 +3,31 @@ const express = require('express')
 // Create express instance
 const app = express()
 
-// Require API routes
-const users = require('./routes/users')
-const test = require('./routes/test')
-
 // Import API Routes
-app.use(users)
-app.use(test)
+app.use('/test', (req, res) => {
+  res.end('Test API!')
+})
+// Mock Users
+const usersData = [
+  { name: 'Alexandre' },
+  { name: 'Pooya' },
+  { name: 'Sébastien' }
+]
+
+/* GET users listing. */
+app.get('/users', function (req, res, next) {
+  res.json(usersData)
+})
+
+/* GET user by ID. */
+app.get('/users/:id', function (req, res, next) {
+  const id = parseInt(req.params.id)
+  if (id >= 0 && id < usersData.length) {
+    res.json(usersData[id])
+  } else {
+    res.sendStatus(404)
+  }
+})
 
 // Export express app
 module.exports = app
