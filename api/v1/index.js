4ad371ab8656dc2d5b6b50901ad388a3c42cbdb6
unknown <imaadecima@gmail.com>
2021-09-06 16:09:57 -0300

    Add path in express nuxt

--- api/v1/index.js
+++ api/v1/index.js
@@ -5,8 +5,23 @@ module.exports = { path: '/api', handler: app }
 
 app.use(bodyParser.json())
 
-app.post('/token', (req, res) => {
+app.post('/', (req, res) => {
+  res.end('API Nuxt')
+})
+
+app.get('/token', (req, res) => {
   const cookies = new Cookies(req, res)
   const token = cookies.get('token')
   res.json(token)
+})
+
+app.post('/token', (req, res) => {
+  const cookies = new Cookies(req, res)
+
+  cookies.set('token', 'Bearer TOKEN', {
+    maxAge: 3600000 * 12,
+    httpOnly: true // true by default
+  })
+
+  res.end('Token guardado')
 })
